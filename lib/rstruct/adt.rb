52e265147add445f933a122e478dac0dfb1d93a2
petitviolet <violethero0820@gmail.com>
2020-01-13 15:55:45 +0900

    Set :name properly to enum and adt

--- lib/rstruct/adt.rb
+++ lib/rstruct/adt.rb
@@ -12,28 +12,15 @@ module ADT
       Rstruct.new(:value, __caller: caller).new(value)
     else
       Rstruct.new(__caller: caller).new
-    end.tap { |k| @@adt_types << k }
+    end.tap do |k|
+      @@adt_types << k
+      def k.name
+        self.class.name
+      end
+    end
   end
 
   def data(*fields)
     Rstruct.new(*fields, __caller: caller).tap { |k| @@adt_types << k.name }
   end
-
-  private
-
-    def build_class_name(caller)
-      names = caller.map do |stack|
-        # ".../hoge.rb:7:in `<module:Hoge>'"
-        if (m = stack.match(/\A.+in `<(module|class):(.+)>.+/))
-          m[2]
-        end
-      end.reject(&:nil?)
-      file_name, line_num = caller[0].split(':')
-      line_executed = File.readlines(file_name)[line_num.to_i - 1]
-      names << line_executed.match(/\A\s*(\S+)\s*=/)[1] # "  Point = Rstruct.new(:x, :y)\n"
-      names.join('::')
-    rescue StandardError
-      'Enum'
-    end
-
 end
--- lib/rstruct/enum.rb
+++ lib/rstruct/enum.rb
@@ -11,7 +11,12 @@ module Enum
     if (type = of(value))
       raise ArgumentError, "Enum '#{value}' already defined in #{type.name}"
     end
-    Rstruct.new(:value, __caller: caller).new(value).tap { |k| @@enum_values << k }
+    Rstruct.new(:value, __caller: caller).new(value).tap do |k|
+      @@enum_values << k
+      def k.name
+        self.class.name
+      end
+    end
   end
 
   def of(value)
