5fd5ad59e05c90894b292ad1c90cc4b7e65eb54c
petitviolet <violethero0820@gmail.com>
2020-01-13 15:56:15 +0900

    Added Enum test

---
+++ test/enum_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class EnumTest < Minitest::Test
+  module Color
+    extend Enum
+    Red = enum 'red'
+    Green = enum 'green'
+    Blue = enum 'blue'
+  end
+
+  def test_it_defines_adt
+    assert_equal Color::Red.name, 'EnumTest::Color::Red'
+    assert_equal Color::Red.value, 'red'
+    assert_equal Color::Green.name, 'EnumTest::Color::Green'
+    assert_raises(NoMethodError) { Color::Red.new }
+
+    case Color.of('blue')
+    in Color::Red
+        assert false
+    in Color::Green
+        assert false
+    in Color::Blue
+        assert true
+    else
+      assert false
+    end
+
+    assert_nil Color.of('orange')
+  end
+end
