09d961edb64bc7578477d01ad5d31e76d374322e
petitviolet <violethero0820@gmail.com>
2020-01-13 16:53:54 +0900

    Renamed test type from Type to Value

--- test/rstruct_test.rb
+++ test/rstruct_test.rb
@@ -1,15 +1,15 @@
 require "test_helper"
 
 class RstructTest < Minitest::Test
-  Type = Rstruct.new(:value)
+  Value = Rstruct.new(:value)
 
   def test_it_defines_struct
-    assert_equal Type.name, 'RstructTest::Type'
-    value = Type.new(100)
+    assert_equal Value.name, 'RstructTest::Value'
+    value = Value.new(100)
     assert_equal value.value, 100
-    assert_equal value, Type.new(100)
+    assert_equal value, Value.new(100)
     case value
-    in Type[n]
+    in Value[n]
         assert_equal n, 100
     else
       assert false
